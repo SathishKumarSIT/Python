170b37ab23f82f1ae222e671922eb7f006e19a7e
SathishKumarSIT <32791298+SathishKumarSIT@users.noreply.github.com>
2021-06-24 20:05:06 +0530

    Create HellloGuys.java

---
+++ HellloGuys.java
@@ -0,0 +1,9 @@
+public class Holidays {
+      public static void main(Strings[] args){
+            //TODO Auto-generated method stub
+            System.out.println("Hai Sathish");
+            System.out.println("Hai Raj");
+            System.out.println("Hai Mahesh");
+            
+       }
+}
